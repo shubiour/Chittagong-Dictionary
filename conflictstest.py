70ca982cf54e4a140e3130ef9a7d75ff7823ada0
Shubiour Shuvo <sv95.shuvo@gmail.com>
2020-06-22 15:19:25 +0600

    Add a this file bcz testing out conflicts between local and remnote

---
+++ conflictstest.py
@@ -0,0 +1,28 @@
+fname = input('Enter file:')
+fh = open(fname)
+
+senderDict = dict()
+senderList = list()
+
+for line in fh:
+    line.rstrip()
+    if line.startswith('From:'):
+        continue
+    if line.startswith('From'):
+        words = line.split()
+        senderList.append(words[1])
+
+for key in senderList:
+     senderDict[key] = senderDict.get(key,0)+1
+
+bigvalue = 0
+bigKey = None
+
+for key,value in senderDict.items():
+    if  value > bigvalue:
+        bigvalue = value
+        bigKey = key
+
+print(bigKey,bigvalue)
+
+
